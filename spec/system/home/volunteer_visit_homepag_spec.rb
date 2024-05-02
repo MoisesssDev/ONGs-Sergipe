69a1be3345e2d1c234a6e1349c4c61863b5af3bb
Moises Almeida <moisesssprg@gmail.com>
2024-05-02 14:32:55 -0300

    test: adiciona teste que garante a mensagem do header

--- spec/system/home/volunteer_visit_homepag_spec.rb
+++ spec/system/home/volunteer_visit_homepag_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 describe 'Voluntario visita a página inicial' do
   it 'e vê a mensagem de boas-vindas' do
     visit root_path
-    expect(page).to have_content('Bem-vindo ao sistema de voluntariado!')
+
+    expect(page).to have_content('Encontre ONGs em Sergipe')
+    expect(page).to have_content('Bem-vindo ao ONGs Sergipe, dedicado a facilitar o acesso às ' \
+                                 'Organizações Não Governamentais (ONGs) em Sergipe.')
+    expect(page).to have_link('Buscar por ONGs')
   end
 end
