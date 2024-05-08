686d547d48d4223b56df54e22979fbf112b9fe7a
Moises Almeida <moisesssprg@gmail.com>
2024-05-07 23:51:35 -0300

    fix: soluciona bug na contagem de ONGs na busca

--- app/controllers/ongs_controller.rb
+++ app/controllers/ongs_controller.rb
@@ -1,14 +1,14 @@
 class OngsController < ApplicationController
   def index
+    @counting = Ong.search(params[:query]).count
+
     @pagy, @ongs = pagy(Ong.all)
   end
 
   def search
-    if params[:query].blank?
-      redirect_to ongs_path
-    else
-      @pagy, @ongs = pagy(Ong.where('name LIKE :query OR description LIKE :query', query: "%#{params[:query]}%"))
-      render :index
-    end
+    @counting = Ong.search(params[:query]).count
+
+    @pagy, @ongs = pagy(Ong.search(params[:query]))
+    render :index
   end
 end
--- app/models/ong.rb
+++ app/models/ong.rb
@@ -1,4 +1,10 @@
 class Ong < ApplicationRecord
   has_one_attached :image
   belongs_to :category
+
+  def self.search(query)
+    return Ong.all if query.blank?
+
+    where('name LIKE :query OR description LIKE :query', query: "%#{query}%")
+  end
 end
