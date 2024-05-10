b00c388f94841ec330627820da1029039b5837a1
Moises Almeida <moisesssprg@gmail.com>
2024-05-10 19:02:04 -0300

    feat: adiciona a lógica de busca por categoria.

--- app/controllers/ongs_controller.rb
+++ app/controllers/ongs_controller.rb
@@ -11,4 +11,11 @@ class OngsController < ApplicationController
     @pagy, @ongs = pagy(Ong.search(params[:query]))
     render :index
   end
+
+  def search_by_category
+    @counting = Ong.search_by_category(params[:category_id]).count
+
+    @pagy, @ongs = pagy(Ong.search_by_category(params[:category_id]))
+    render :index
+  end
 end
