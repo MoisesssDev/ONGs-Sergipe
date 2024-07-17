23d6feeb17a9f61dad676249f834e9d439de2e97
Moises Almeida <moisesssprg@gmail.com>
2024-07-17 14:18:31 -0300

    feat: adiciona logica que implementa pagina de detalhes da ong

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :ongs, only: %i[index] do
+  resources :ongs, only: %i[index show] do
     get 'search', on: :collection
     get 'category/:category_id', on: :collection, action: :search_by_category, as: :category
   end
